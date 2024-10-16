f5c12771d81f4d6c6d9a76b3e1fcbd5b234fac27
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-10-15 21:13:16 -0400

    Stored GET Request Results Into An Array Instead of Displaying

--- frontend/src/components/AccessibleStations.jsx
+++ frontend/src/components/AccessibleStations.jsx
@@ -3,38 +3,35 @@ import {fetchHandler} from '../utils/fetchingUtils.js';
 
 export default function AccessibleStations() {
   const [accessibleTrainStations, setTrainStations] = useState([]);
+  const [accessibleArr, setAccessibleArr] = useState([]);
 
   useEffect(() => {
     const doFetch = async () => {
       const API_URL = `https://data.ny.gov/resource/39hk-dx4f.json?ada=1`;
       try {
-        const [data, error] = await fetchHandler(API_URL);
-        if (data) setTrainStations(data);
-        console.log("HELLLOOOOOO");
-        console.log(data);
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+
+        // Push data into accessibleArr
+        const newAccessibleArr = [];
+        data.forEach(trainStation => {
+          newAccessibleArr.push(trainStation);
+        });
+
+        // Update state
+        setTrainStations(data);
+        setAccessibleArr(newAccessibleArr);
       } catch (error) {
         console.log(error.message);
       }
-    }
+    };
+    
     doFetch();
-  }, [])
+  }, []);
 
-  return (
-    <div>
-      <h1>Accessible Stations</h1>
-      <ul>
-        {
-          accessibleTrainStations.length > 0
-            ? accessibleTrainStations.map((trainStation) => (
-              <li key={trainStation.complex_id}>
-                <p>Station ID: {trainStation.station_id}</p>
-                <p>Stop Name: {trainStation.stop_name}</p>
-              </li>
-            ))
-            : <p>Train Stations Are Not Found</p>
-        }
-      </ul>
-    </div>
-  );
+  console.log("Accessible Array: ", accessibleArr); // This will show the pushed data 
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import AccessibleStations from '../components/AccessibleStations';
 export default function HomePage() {
   return <>
     <h1>Home</h1>
+    <p>Cool stuff goes here!</p>
     <AccessibleStations></AccessibleStations>
   </>
 };
