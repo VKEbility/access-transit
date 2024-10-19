201bc16f0044df8eb62bf524da2f048c00a75d77
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-10-19 06:41:14 -0400

    Only Subway Alerts: Fetched and Rendered the Necessary Properties

--- frontend/src/components/SubwayAlerts.jsx
+++ frontend/src/components/SubwayAlerts.jsx
@@ -21,19 +21,23 @@ export default function SubwayAlerts() {
         
         // Push data into newSubwayAlertObj 
         data.entity.forEach((subwayAlert) => {
+          // header and description texts 
           const headerText = subwayAlert.alert.header_text?.translation?.[0]?.text || 'No Header Text';
           const descriptionText = subwayAlert.alert.description_text?.translation?.[0]?.text || 'No Description Text';
+          // alert metadata 
           const alertType = subwayAlert.transit_realtime?.mercury_alert?.alert_type || 'No Alert Type';
           const createdAt = subwayAlert.transit_realtime?.mercury_alert?.created_at || 'No Created-At';
           const updatedAt = subwayAlert.transit_realtime?.mercury_alert?.updated_at || 'No Updated-At';
+          // human-readable active period
           const textActivePeriod = subwayAlert.transit_realtime?.mercury_alert?.human_readable_active_period?.translation?.[0]?.text || 'No Text Active Period';
+          // UNIX active period 
           const unixStartTime = subwayAlert.alert.active_period?.start || 'No Unix Start Time';
           const unixEndTime = subwayAlert.alert.active_period?.end || 'No Unix End Time';
+          // informed entities 
           const agencyEntity = subwayAlert.informed_entity?.[0]?.agency_id || 'No Agency ID';
           const stopIdEntity = subwayAlert.informed_entity?.[1]?.stop_id || 'No Stop ID';
           const routeIdEntity = subwayAlert.informed_entity?.[0]?.route_id || 'No Route ID'; 
           const sortOrderEntity = subwayAlert.informed_entity?.[0]?.transit_realtime?.mercury_entity_selector?.sort_order || 'No Sort Order';
-
 
 
           newSubwayAlertObj[subwayAlert.id] = [headerText, descriptionText, alertType, createdAt, updatedAt, textActivePeriod, unixStartTime, unixEndTime, 
@@ -59,14 +63,16 @@ export default function SubwayAlerts() {
           
           Object.entries(subwayAlertsObj).map(([id, specifics]) => {
             // destructure 
-            const [headerText, descriptionText, alertType, createdAt] = specifics;
+            const [headerText, descriptionText, alertType, createdAt, updatedAt, textActivePeriod, routeIdEntity] = specifics;
             return (
               <li key={id}>
-                <p>ID: {id}</p>
-                <p>Header Text: {headerText}</p>
-                <p>Description Text: {descriptionText}</p>
+                <p>Header: {headerText}</p>
+                <p>Description: {descriptionText}</p>
                 <p>Alert Type: {alertType}</p>
                 <p>Created At: {createdAt}</p> 
+                <p>Updated At: {updatedAt}</p> 
+                <p>Active Period: {textActivePeriod}</p> 
+                <p>Route ID: {routeIdEntity}</p> 
               </li>
             );
           })
