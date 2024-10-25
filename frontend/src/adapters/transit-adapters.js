1c59e7ff4729fb70a29e6d56c180b80d87d19bf2
eileectrxity <git@eileenrodriguez.com>
2024-10-25 16:59:34 -0400

    feat: add frontend adapter for specific route accessibility status #33

--- frontend/src/adapters/transit-adapters.js
+++ frontend/src/adapters/transit-adapters.js
@@ -7,3 +7,9 @@ export const fetchNearbyRoutes = async (coords) => {
   if (err) return [null, err.msg];
   return [data, null];
 };
+
+export const getRouteAccessibility = async (rt_stop_id) => {
+  const [data, err] = await fetchHandler(`${baseUrl}/${rt_stop_id}/accessibility`);
+  if (err) return [null, err.msg];
+  return [data, null];
+};
