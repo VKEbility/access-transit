937564168dad2deacfe146571f772dd885166c03
eileectrxity <git@eileenrodriguez.com>
2024-10-25 14:21:32 -0400

    feat: implement DOMPurify for input sanitization and escaping
    - Added DOMPurify to sanitize user inputs and prevent XSS attacks.
    - To ensure all inputs can be processed safely before handling.

--- frontend/src/components/Map/LocationSearch.jsx
+++ frontend/src/components/Map/LocationSearch.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { fetchSearchLocation } from '../../adapters/map-adapters';
+import sanitizeInput from '../../utils/validation';
 import '../../styles/map.css';
 
 export default function LocationSearch({ setCoords, setLocationSearched }) {
@@ -7,7 +8,8 @@ export default function LocationSearch({ setCoords, setLocationSearched }) {
   const [errorText, setErrorText] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const formattedQuery = encodeURIComponent(query.trim());
+  const sanitizedQuery = sanitizeInput(query); //escaping + sanitizing to prevent xss attacks
+  const formattedQuery = encodeURIComponent(sanitizedQuery.trim());
   console.log('Search query at @LocationSearch.jsx:', formattedQuery);  //to help debug
 
   const loadQueryCoords = async (e) => {
@@ -22,11 +24,11 @@ export default function LocationSearch({ setCoords, setLocationSearched }) {
     if (locations && locations.length > 0) {
       const { lat, lon } = locations[0]; //getting coords for first search result (cons: won't be as accurate as user choosing from autocomplete options)
       setCoords({ lat, lon }); //updating coords state
-      setLocationSearched(true);
+      setLocationSearched(true); //invoking the useEffect in useNearbyRoutes hook
       setQuery(''); //clear input field
       setLoading(false);
     }
-    setLocationSearched(false);
+    setLocationSearched(false); //resetting state at the end
   };
 
   return (
