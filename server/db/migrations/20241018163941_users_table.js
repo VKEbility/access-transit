85e823766ffa02f55b00410f48e9bc5e923a508b
Eileen Rodriguez <git@eileenrodriguez.com>
2024-10-28 13:03:47 -0400

    feat: update migrations and seeds for accessibility status and hero actions (#58)
    - Refactored the accessibility_status migrations.
    - Updated hero_actions migrations.
    - Adjusted seeds to accommodate for new implementation strategies.

--- server/db/migrations/20241018163941_users_table.js
+++ server/db/migrations/20241018163941_users_table.js
@@ -5,12 +5,11 @@
 exports.up = (knex) => {
   return knex.schema.createTable('users', (table) => {
     table.increments();
-    table.string('username').notNullable().unique();
+    table.string('username').notNullable().unique(); //unique username
     table.string('password_hash').notNullable();
-    table.string('email').notNullable().unique();
-    table.string('language').notNullable();
-    table.integer('hero_count').notNullable().defaultTo(0);
-    table.timestamps(true, true); //to keep track of time updated
+    table.string('email').notNullable().unique(); //unique email
+    table.string('language').notNullable(); //language codes: es, en
+    table.timestamps(true, true); //to keep track of time updated- automatically created when a new field is inserted
   })
 };
 
