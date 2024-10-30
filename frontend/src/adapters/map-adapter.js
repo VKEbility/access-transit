01d6e3e9dc13c2debaf360d8e1f17598ed35a46a
eileectrxity <git@eileenrodriguez.com>
2024-10-30 06:01:50 -0400

    chore: updating main with latest changes across all branches

--- frontend/src/adapters/map-adapter.js
+++
@@ -1,9 +0,0 @@
-import { fetchHandler, getPostOptions } from '../../../shared/fetchingUtils.mjs';
-
-const baseUrl = '/api';
-
-export const getSearchLocation = async (query) => {
-  const [data, err] = await fetchHandler(`${baseUrl}/map-search`, getPostOptions(query));
-  if (err) return [null, err.msg];
-  return [data, null];
-};
