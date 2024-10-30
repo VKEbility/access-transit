c3506ab436f1170968e3c8f1031cb1bf2fb22a5e
eileectrxity <git@eileenrodriguez.com>
2024-10-30 19:13:15 -0400

    style: init mantine ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,21 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next'; //language translator
+import { MantineProvider } from '@mantine/core'; //ui
 import App from './App.jsx';
 import UserContextProvider from './contexts/CurrentUserContextProvider.jsx';
-import './styles/index.css';
-
-//language translator
-import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+import './styles/index.css'; //global styles
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<UserContextProvider>
-		<BrowserRouter>
-			<I18nextProvider i18n={i18n}>
-				<App />
-			</I18nextProvider>
-		</BrowserRouter>
-	</UserContextProvider>
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <UserContextProvider>
+      <BrowserRouter>
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
+      </BrowserRouter>
+    </UserContextProvider>
+  </MantineProvider>
 );
