ad8163bc525d36cd2da726db48aa1f70a8988e3f
eileectrxity <git@eileenrodriguez.com>
2024-10-30 18:52:41 -0400

    feat: init itinerary direction functionality
    -Updating TransitRouteCard component to support swiping between uptown and downtown views.

--- frontend/src/components/TransitRoutes/TransitRouteCard.jsx
+++ frontend/src/components/TransitRoutes/TransitRouteCard.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import RouteIcon from './RouteIcon';
 import AccessibilityStatusContainer from './AccessibilityStatusContainer';
 import CountdownTimer from '../../hooks/CountdownTimer';
 import '../../styles/routes.css';
 
 export default function TransitRouteCard({ route, onTimerEnd }) {
+  const [currDirection, setCurrDirection] = useState(0); //state for direction swipe- 0 for uptown, 1 for downtown
+
   const itineraries = route.itineraries || [];
-  const closestStop = itineraries[0]?.closestStop || {}; //rendering uptown stop for now
-  const realtimeArrival = itineraries[0]?.realTimeArrivals[0] || {};
+  const uptown = itineraries.find(it => it.directionId === 0) || {}; //obj { closestStop{}, directionHeadsign, directionId, headsign, mergedHeadsign, closestStop, realTimeArrivals }
+  const downtown = itineraries.find(it => it.directionId === 1) || {};
+
+  const currItinerary = currDirection === 0 ? uptown : downtown;
+  const closestStop = currItinerary.closestStop || {}; //rendering the closest stop of whichever itinerary view currDirection state is on
+  const realtimeArrival = currItinerary.realTimeArrivals[0] || {};
   const departure = realtimeArrival?.departureTime;
   const longName = route.longName || '';
   const isOutOfCommission = !realtimeArrival || Object.keys(realtimeArrival).length === 0;
