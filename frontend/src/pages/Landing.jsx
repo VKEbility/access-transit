422725c0c474199db3b5039ea3d1fd94167da9c5
eileectrxity <git@eileenrodriguez.com>
2024-11-01 10:20:26 -0400

    final

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -7,63 +7,63 @@ import { Grid, Title, Alert } from '@mantine/core';
 
 const MapContainerComponent = React.lazy(() => import('../components/Map/Map'));
 const TransitRouteContainer = React.lazy(() =>
-	import('../components/TransitRoutes/TransitRouteContainer')
+  import('../components/TransitRoutes/TransitRouteContainer')
 );
 
 export default function LandingPage() {
-	const [coords, setCoords] = useState({ lat: 40.7128, lon: -74.006 }); //default coordinates set to nyc
-	const [mapReady, setMapReady] = useState(false);
-	const [locationSearched, setLocationSearched] = useState(false);
+  const [coords, setCoords] = useState({ lat: 40.7128, lon: -74.006 }); //default coordinates set to nyc
+  const [mapReady, setMapReady] = useState(false);
+  const [locationSearched, setLocationSearched] = useState(false);
 
-	const { loadNearbyRoutes } = useNearbyRoutes(
-		coords,
-		mapReady,
-		locationSearched
-	);
-	const LoadingFallback = () => <div>Loading...</div>;
+  const { loadNearbyRoutes } = useNearbyRoutes(
+    coords,
+    mapReady,
+    locationSearched
+  );
+  const LoadingFallback = () => <div>Loading...</div>;
 
-	useEffect(() => {
-		const script = document.createElement('script');
-		script.src =
-			'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-		script.async = true;
-		document.body.appendChild(script);
-		//init the google translator
-		window.googleTranslateElementInit = () => {
-			new window.google.translate.TranslateElement(
-				{
-					pageLanguage: 'en',
-					includedLanguages: 'en,es,fr,de,it,ja,zh,pt,ru,ar,hi,ko',
-					layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
-				},
-				'google_translate_element'
-			);
-		};
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    script.async = true;
+    document.body.appendChild(script);
+    //init the google translator
+    window.googleTranslateElementInit = () => {
+      new window.google.translate.TranslateElement(
+        {
+          pageLanguage: 'en',
+          includedLanguages: 'en,es,fr,de,it,ja,zh,pt,ru,ar,hi,ko',
+          layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+        },
+        'google_translate_element'
+      );
+    };
 
-		return () => {
-			document.body.removeChild(script);
-			delete window.googleTranslateElementInit;
-		};
-	}, []);
+    return () => {
+      document.body.removeChild(script);
+      delete window.googleTranslateElementInit;
+    };
+  }, []);
 
-	return (
-		<>
-			<Suspense fallback={<LoadingFallback />}>
-				{/* Add the translator container */}
-				<TransitHeader />
-				<MapContainerComponent
-					coords={coords}
-					setCoords={setCoords}
-					setMapReady={setMapReady}
-				/>
-				<LocationSearch
-					setCoords={setCoords}
-					setLocationSearched={setLocationSearched}
-				/>
-				<FavoriteContainer></FavoriteContainer>
-				<TransitRouteContainer coords={coords} mapReady={mapReady} />
-				<div id="google_translate_element"></div>{' '}
-			</Suspense>
-		</>
-	);
+  return (
+    <>
+      <Suspense fallback={<LoadingFallback />}>
+        {/* Add the translator container */}
+        <TransitHeader />
+        <MapContainerComponent
+          coords={coords}
+          setCoords={setCoords}
+          setMapReady={setMapReady}
+        />
+        <LocationSearch
+          setCoords={setCoords}
+          setLocationSearched={setLocationSearched}
+        />
+        <FavoriteContainer></FavoriteContainer>
+        <TransitRouteContainer coords={coords} mapReady={mapReady} />
+        <div id="google_translate_element"></div>{' '}
+      </Suspense>
+    </>
+  );
 }
